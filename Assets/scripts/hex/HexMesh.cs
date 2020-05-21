b401cccab83af6d3c434f0359649d35485cafaaa
delevin <846353427@qq.com>
2020-05-21 15:25:17 +0800

    颜色重合

--- Assets/scripts/hex/HexMesh.cs
+++ Assets/scripts/hex/HexMesh.cs
@@ -61,8 +61,15 @@ public class HexMesh : MonoBehaviour
 
         AddTriangle(center, v1, v2);
         AddTriangleColor(cell.color, cell.color, cell.color);
+        if (hexDirection == HexDirection.NE)
+        {
+            TriangulateConnection(hexDirection, cell, v1, v2);
+        }
+        else if (hexDirection <= HexDirection.SE)
+        {
+            TriangulateConnection(hexDirection, cell, v1, v2);
+        }
 
-        TriangulateConnection(hexDirection, cell, v1, v2);
 
         // Vector3 bridge = HexMetrics.GetBridge(hexDirection);
         // Vector3 v3 = v1 + bridge;
@@ -89,13 +96,21 @@ public class HexMesh : MonoBehaviour
 
     private void TriangulateConnection(HexDirection direction, HexCell cell, Vector3 v1, Vector3 v2)
     {
-        HexCell neighbor = cell.GetNeighbor(direction) ?? cell;
+        HexCell neighbor = cell.GetNeighbor(direction);
+        if (neighbor == null) return;
         Vector3 bridge = HexMetrics.GetBridge(direction);
         Vector3 v3 = v1 + bridge;
         Vector3 v4 = v2 + bridge;
 
         AddQuad(v1, v2, v3, v4);
         AddQuadColor(cell.color, neighbor.color);
+
+        HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
+        if (direction <= HexDirection.E && nextNeighbor != null)
+        {
+            AddTriangle(v2, v4, v2 + HexMetrics.GetBridge(direction.Next()));
+            AddTriangleColor(cell.color, neighbor.color, nextNeighbor.color);
+        }
     }
 
     private void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
